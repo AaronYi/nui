c8dc4b057d036f924e31c4829c1341a9fc9b4520
AaronYi <zhuolin.yi@gmail.com>
2014-04-30 17:28:47 +0800

    optimize

--- NUI/Core/Renderers/NUIViewRenderer.m
+++ NUI/Core/Renderers/NUIViewRenderer.m
@@ -13,12 +13,13 @@
 + (void)render:(UIView*)view withClass:(NSString*)className
 {
     if ([NUISettings hasProperty:@"background-image" withClass:className]) {
-        UIImage *image = [NUISettings getImage:@"background-image" withClass:className];
-        view.layer.contents = (id)image.CGImage;
+        if ([NUISettings hasProperty:@"background-repeat" withClass:className] && ![NUISettings getBoolean:@"background-repeat" withClass:className]) {
+            view.layer.contents = (__bridge id)[NUISettings getImage:@"background-image" withClass:className].CGImage;
+        } else {
+            [view setBackgroundColor: [NUISettings getColorFromImage:@"background-image" withClass: className]];
+        }
     } else if ([NUISettings hasProperty:@"background-color" withClass:className]) {
         [view setBackgroundColor: [NUISettings getColor:@"background-color" withClass: className]];
-    } else if ([NUISettings hasProperty:@"background-image-pattern" withClass:className]) {
-        [view setBackgroundColor: [NUISettings getColorFromImage:@"background-image-pattern" withClass: className]];
     }
 
     [self renderSize:view withClass:className];
@@ -29,15 +30,15 @@
 + (void)renderBorder:(UIView*)view withClass:(NSString*)className
 {
     CALayer *layer = [view layer];
-    
+
     if ([NUISettings hasProperty:@"border-color" withClass:className]) {
         [layer setBorderColor:[[NUISettings getColor:@"border-color" withClass:className] CGColor]];
     }
-    
+
     if ([NUISettings hasProperty:@"border-width" withClass:className]) {
         [layer setBorderWidth:[NUISettings getFloat:@"border-width" withClass:className]];
     }
-    
+
     if ([NUISettings hasProperty:@"corner-radius" withClass:className]) {
         [layer setCornerRadius:[NUISettings getFloat:@"corner-radius" withClass:className]];
     }
@@ -46,19 +47,19 @@
 + (void)renderShadow:(UIView*)view withClass:(NSString*)className
 {
     CALayer *layer = [view layer];
-    
+
     if ([NUISettings hasProperty:@"shadow-radius" withClass:className]) {
         [layer setShadowRadius:[NUISettings getFloat:@"shadow-radius" withClass:className]];
     }
-    
+
     if ([NUISettings hasProperty:@"shadow-offset" withClass:className]) {
         [layer setShadowOffset:[NUISettings getSize:@"shadow-offset" withClass:className]];
     }
-    
+
     if ([NUISettings hasProperty:@"shadow-color" withClass:className]) {
         [layer setShadowColor:[NUISettings getColor:@"shadow-color" withClass:className].CGColor];
     }
-    
+
     if ([NUISettings hasProperty:@"shadow-opacity" withClass:className]) {
         [layer setShadowOpacity:[NUISettings getFloat:@"shadow-opacity" withClass:className]];
     }
@@ -70,7 +71,7 @@
     if ([NUISettings hasProperty:@"height" withClass:className]) {
         height = [NUISettings getFloat:@"height" withClass:className];
     }
-    
+
     CGFloat width = view.frame.size.width;
     if ([NUISettings hasProperty:@"width" withClass:className]) {
         width = [NUISettings getFloat:@"width" withClass:className];
